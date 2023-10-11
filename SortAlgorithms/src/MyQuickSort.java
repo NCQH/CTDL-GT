6d9d6949821aa01d097a4f546e6dbbd90ba2e3bc
NCQH <huy181103@gmail.com>
2023-10-11 21:26:53 +0700

    Sort Algorithm

--- SortAlgorithms/src/MyQuickSort.java
+++ SortAlgorithms/src/MyQuickSort.java
@@ -1,2 +1,28 @@
 public class MyQuickSort {
+    public static void sort(Comparable[] a) {
+        MyShuffing.shuffle(a);;
+        sort(a, 0, a.length - 1);
+    }
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo, j = hi + 1;
+        while (true) {
+            while (less(a[++i], a[lo]))
+                if (i == hi) break;
+
+            while (less(a[lo], a[--j]))
+                if (j == lo) break;
+
+            if (i >= j) break;
+            exch(a, i, j);
+        }
+
+        exch(a, lo, j);
+        return j;
+    }
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j-1);
+        sort(a, j+1, hi);
+    }
 }
