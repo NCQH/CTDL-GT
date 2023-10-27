36d2e61a1a1a22a7a448de8279b8ec028bae42b8
NCQH <huy181103@gmail.com>
2023-10-27 21:07:23 +0700

    BTS update:

--- AVLTree/src/BTS.java
+++ AVLTree/src/BTS.java
@@ -1,6 +1,7 @@
+import java.util.Queue;
+
 public class BTS<Key extends Comparable<Key>, Value> {
     private Node root;
-
     private class Node {
         private Key key;
         private Value val;
@@ -18,19 +19,25 @@ public class BTS<Key extends Comparable<Key>, Value> {
         if (x == null) return 0;
         return x.count;
     }
+    public int rank(Key key) {
+        return rank(key, root);
+    }
+    private int rank(Key key, Node x) {
+        if (x == null) return 0;
+        int cmp = key.compareTo(x.key);
+        if  (cmp < 0) return rank(key, x.left);
+        else if  (cmp > 0) return 1 + size(x.left) + rank(key, x.right);
+        else if (cmp == 0) return size(x.left);
+    }
     public void put(Key key, Value val) {
         root = put(root, key, val);
     }
     private Node put(Node x, Key key, Value val) {
         if (x == null) return new Node(key, val);
-        int cmp = key.compareTo(x.key);
-        if (cmp < 0) {
-            x.left = put(x.left, key, val);
-        } else if (cmp > 0) {
-            x.right = put(x.right, key, val);
-        } else if (cmp == 0) {
-            x.val = val;
-        }
+        int cmp = key.compareTo((x.key));
+        if      (cmp < 0)   x.left = put(x.left, key, val);
+        else if (cmp > 0)   x.right = put(x.right, key, val);
+        else if (cmp == 0)  x.val = val;
         x.count = 1 + size(x.left) + size(x.right);
         return x;
     }
@@ -38,33 +45,11 @@ public class BTS<Key extends Comparable<Key>, Value> {
         Node x = root;
         while (x != null) {
             int cmp = key.compareTo(x.key);
-            if (cmp < 0) x = x.left;
+            if  (cmp < 0) x = x.left;
             else if (cmp > 0) x = x.right;
             else if (cmp == 0) return x.val;
         }
         return null;
-    }
-    public void delete(Key key) {
-        root = delete(root, key);
-    }
-    private Node delete(Node x, Key key) {
-        if (x == null) return null;
-        int cmp = key.compareTo(x.key);
-        if (cmp < 0) x.left = delete(x.left, key);
-        else if (cmp > 0) x.right = delete(x.right, key);
-        else {
-            if (x.left == null) return x.left;
-            if (x.right == null) return x.right;
-
-            Node t = x;
-            x = min(t.right);
-            x.right = deleteMin(t.right);
-            x.left = t.left;
-        }
-        x.count = size(x.left) + size(x.right) + 1;
-    }
-    public Iterable<Key> iterator() {
-
     }
     public Key floor(Key key) {
         Node x = floor(root, key);
@@ -74,8 +59,10 @@ public class BTS<Key extends Comparable<Key>, Value> {
     private Node floor(Node x, Key key) {
         if (x == null) return null;
         int cmp = key.compareTo(x.key);
-        if (cmp < 0) return floor(x.left, key);
+
         if (cmp == 0) return x;
+
+        if (cmp < 0) return floor(x.left, key);
 
         Node t = floor(x.right, key);
         if (t != null) return t;
@@ -86,5 +73,45 @@ public class BTS<Key extends Comparable<Key>, Value> {
     }
     private Node ceiling(Node x, Key key) {
 
+    }
+    public void deleteMin() {
+        root = deleteMin(root);
+    }
+    private Node deleteMin(Node x) {
+        if (x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        x.count = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+    public void delete(Key key) {
+        root = delete(root, key);
+    }
+    private Node delete(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if  (cmp < 0) x.left = delete(x.left, key);
+        else if (cmp > 0) x.right = delete(x.right, key);
+        else {
+            if (x.right == null) return x.left;
+            if (x.left == null) return x.right;
+
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.count = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+    public Iterable<Key> iterator() {
+        Queue<Key> q = new Queue<Key>();
+        inorder(root, q);
+        return q;
+    }
+    private void inorder(Node x, Queue<Key> q) {
+        if (x == null) return;
+        inorder(x.left, q);
+        q.add(x.key);
+        inorder(x.right, q);;
     }
 }
---
+++ RedBlackBSTs/src/RedBlackBSTs.java
@@ -0,0 +1,7 @@
+public class RedBlackBSTs<Key extends Comparable<Key>, Value> {
+    private class Node {
+        private Key key;
+        private Value val;
+
+    }
+}
