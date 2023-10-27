19657f76b257044fa75f2b73bc57a38f08f85635
NCQH <huy181103@gmail.com>
2023-10-27 18:05:37 +0700

    BTS update:

--- AVLTree/src/BTS.java
+++ AVLTree/src/BTS.java
@@ -5,23 +5,86 @@ public class BTS<Key extends Comparable<Key>, Value> {
         private Key key;
         private Value val;
         private Node left, right;
-
+        private int count;
         public Node(Key key, Value val) {
             this.key = key;
             this.val = val;
         }
     }
-
+    public int size() {
+        return size(root);
+    }
+    private int size(Node x) {
+        if (x == null) return 0;
+        return x.count;
+    }
     public void put(Key key, Value val) {
-
+        root = put(root, key, val);
+    }
+    private Node put(Node x, Key key, Value val) {
+        if (x == null) return new Node(key, val);
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, val);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, val);
+        } else if (cmp == 0) {
+            x.val = val;
+        }
+        x.count = 1 + size(x.left) + size(x.right);
+        return x;
     }
     public Value get(Key key) {
-
+        Node x = root;
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+            if (cmp < 0) x = x.left;
+            else if (cmp > 0) x = x.right;
+            else if (cmp == 0) return x.val;
+        }
+        return null;
     }
     public void delete(Key key) {
+        root = delete(root, key);
+    }
+    private Node delete(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = delete(x.left, key);
+        else if (cmp > 0) x.right = delete(x.right, key);
+        else {
+            if (x.left == null) return x.left;
+            if (x.right == null) return x.right;
 
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.count = size(x.left) + size(x.right) + 1;
     }
     public Iterable<Key> iterator() {
+
+    }
+    public Key floor(Key key) {
+        Node x = floor(root, key);
+        if (x == null) return null;
+        return x.key;
+    }
+    private Node floor(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return floor(x.left, key);
+        if (cmp == 0) return x;
+
+        Node t = floor(x.right, key);
+        if (t != null) return t;
+        else return x;
+    }
+    public Key ceiling(Key key) {
+
+    }
+    private Node ceiling(Node x, Key key) {
 
     }
 }
